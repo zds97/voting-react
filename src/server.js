77b9b463f888dd2094ef484b00ce8d2aaa35826a
Zach Sebag <zsebag@gmail.com>
2015-12-25 22:21:44 -0600

    Added basic server logic to retrieve state

--- src/server.js
+++ src/server.js
@@ -2,4 +2,11 @@ import Server from 'socket.io';
 
 export default function startServer() {
   const io = new Server().attach(8090);
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
+
 }
